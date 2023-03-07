44d98fb6193f5daa740257e0817cd3533c76e89d
raihan.k.abdillah@outlook.com <raihan.k.abdillah@outlook.com>
2023-03-07 23:07:02 +0800

    Add project files.

---
+++ InternationalWidgets-webapp/Controllers/PurchasesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InternationalWidgets_webapp;
+
+namespace InternationalWidgets_webapp.Controllers
+{
+    public class PurchasesController : Controller
+    {
+        private InternationalWidgetsEntities db = new InternationalWidgetsEntities();
+
+        // GET: Purchases
+        public ActionResult Index()
+        {
+            var purchases = db.Purchases.Include(p => p.Invoice).Include(p => p.Item);
+            return View(purchases.ToList());
+        }
+
+        // GET: Purchases/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = db.Purchases.Find(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // GET: Purchases/Create
+        public ActionResult Create()
+        {
+            ViewBag.InvoiceNo = new SelectList(db.Invoices, "InvoiceNo", "InvoiceNo");
+            ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Description");
+            return View();
+        }
+
+        // POST: Purchases/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PurchaseId,InvoiceNo,ItemId,Quantity,Amount")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Purchases.Add(purchase);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.InvoiceNo = new SelectList(db.Invoices, "InvoiceNo", "InvoiceNo", purchase.InvoiceNo);
+            ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Description", purchase.ItemId);
+            return View(purchase);
+        }
+
+        // GET: Purchases/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = db.Purchases.Find(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.InvoiceNo = new SelectList(db.Invoices, "InvoiceNo", "InvoiceNo", purchase.InvoiceNo);
+            ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Description", purchase.ItemId);
+            return View(purchase);
+        }
+
+        // POST: Purchases/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PurchaseId,InvoiceNo,ItemId,Quantity,Amount")] Purchase purchase)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(purchase).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.InvoiceNo = new SelectList(db.Invoices, "InvoiceNo", "InvoiceNo", purchase.InvoiceNo);
+            ViewBag.ItemId = new SelectList(db.Items, "ItemId", "Description", purchase.ItemId);
+            return View(purchase);
+        }
+
+        // GET: Purchases/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Purchase purchase = db.Purchases.Find(id);
+            if (purchase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(purchase);
+        }
+
+        // POST: Purchases/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Purchase purchase = db.Purchases.Find(id);
+            db.Purchases.Remove(purchase);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ InternationalWidgets-webapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InternationalWidgets_webapp.MvcApplication" Language="C#" %>
